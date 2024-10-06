884ce4316cd6ea9e9f10151931ad00eb4be9000c
Henry Cho <hyunmook@qti.qualcomm.com>
2024-10-06 21:44:45 +0900

    'add alias

---
+++ eza.sh
@@ -0,0 +1,8 @@
+# alias eza
+alias ls='eza -l --icons --git --group-directories-last --color=always --sort=name'
+alias la='eza -al --icons --git --group-directories-last --color=always --sort=name'
+alias la='eza -al --icons --git --group-directories-last --color=always --sort=modified'
+alias ld='eza -lD --icons --git --color=always'
+alias lf='eza -lf --icons --git --color=always'
+alias lh='eza -dl .* --group-directories-first --icons --git'
+alias lt='eza -al --icons --git --color=always --sort=modified'
---
+++ fzf-git.sh
@@ -0,0 +1,3 @@
+# alias fzf for git
+
+alias br='git br | fzf --height=20% --reverse --info=inline | xargs git sw'
